b2fa20f9ad2724efc78941c749f1e4ad3ee008f9
JoseAFloresA24 <67332426+JoseAFloresA24@users.noreply.github.com>
2022-10-17 07:25:08 -0700

    adding reponsebody test

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -4,7 +4,12 @@ test.describe.parallel("API Testing", () => {
     const baseURL = ""
 
     test("GET", async ({request}) => {
-        const response = await request.get("https://reqres.in/api/users/3")
+        const response = await request.get("https://reqres.in/api/users/1")
+        const responseBody = JSON.parse(await response.text())
+        
         expect(response.status()).toBe(200)
+
+        expect(responseBody.data.id).toBe(1)
+        expect(responseBody.data.first_name).toBe("George")
     })
 })
